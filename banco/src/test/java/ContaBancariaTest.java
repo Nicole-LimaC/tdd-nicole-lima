c9503f88502f459f4ab5799869a009139d0ed145
Nicole Lima Crispim <nicolelima1209@gmail.com>
2025-11-07 21:55:15 -0300

    criar teste para sacar

--- banco/src/test/java/ContaBancariaTest.java
+++ banco/src/test/java/ContaBancariaTest.java
@@ -9,4 +9,12 @@ public class ContaBancariaTest {
         conta.depositar(100);
         assertEquals(100, conta.getSaldo(), 0.001);
     }
+
+    @Test
+    public void deveSacarValor() {
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(100);
+        conta.sacar(40);
+        assertEquals(60, conta.getSaldo(), 0.001);
+    }
 }
